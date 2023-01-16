758ee2d037243c645f93d3222101cf59a0d29075
Galajus <galajus@gmail.com>
2023-01-16 12:41:10 +0100

    better routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {CalculatorComponent} from "./calculator/calculator.component";
 import {BrokencalcComponent} from "./brokencalc/brokencalc.component";
 import {HomeComponent} from "./home/home.component";
 import {DefaultComponent} from "./layouts/default/default.component";
+import {LoginComponent} from "./account/login/login.component";
+import {DashboardComponent} from "./account/dashboard/dashboard.component";
+import {DiscordApiRedirectComponent} from "./account/login/discord-api-redirect/discord-api-redirect.component";
+import {PageNotFoundComponent} from "./error/page-not-found/page-not-found.component";
+import {DiscordAuthComponent} from "./account/login/auth/discord/discord-auth.component";
 
 const routes: Routes = [
 
@@ -11,9 +15,18 @@ const routes: Routes = [
     path:'', component: DefaultComponent, children: [
       { path: '', component: HomeComponent },
       { path: 'br', component: BrokencalcComponent },
-      { path: 'build-calculator', component: BrokencalcComponent }
+      { path: 'build-calculator', component: BrokencalcComponent },
+      /*{ path: 'login', component: LoginComponent },
+      { path: 'login/discord-api', component: DiscordApiRedirectComponent },
+      { path: 'login/auth/discord', component: DiscordAuthComponent },
+
+      { path: 'acc', component: DashboardComponent },
+      { path: 'acc/dashboard', component: DashboardComponent },*/
+
+      { path: '**', component: PageNotFoundComponent}
       ]
-  }
+
+  },
 
 ];
 
