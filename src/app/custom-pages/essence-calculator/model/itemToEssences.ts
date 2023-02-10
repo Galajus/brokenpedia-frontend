c03b97270cf2ee8977190048d2e30e3b1058b64f
Galajus <galajus@gmail.com>
2023-02-10 12:39:50 +0100

    more more more - bad practice :c

---
+++ src/app/custom-pages/essence-calculator/model/itemToEssences.ts
@@ -0,0 +1,9 @@
+export interface ItemToEssences {
+  rank: string,
+  inhiPrice: number,
+  essences: number,
+  essencesWithInhi: number,
+  priceThreshold: number,
+  priceThresholdWithInhi: number,
+  incomeWithInhi: number
+}
